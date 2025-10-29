f666a00815d165ecc5af9fdeb030340c08005fdf
sazzh <100480867+sazzh@users.noreply.github.com>
2025-10-30 02:52:44 +1100

    Add loading bar

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function showNumber() {
     var x = document.getElementById("inputNumber");
     document.getElementById("numberDisplay").innerText = x.value;
+
+    // run loading bar
+    const loadingBar = document.getElementById("loadingBar");
+    var width = 1;
+    var id = setInterval(frame, 15);
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            loadingBar.style.width = width + '%';
+        }
+    }
 }
