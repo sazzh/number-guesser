47247ce163f14ed727a7322d7de3e42a5c3927c6
sazzh <100480867+sazzh@users.noreply.github.com>
2025-10-30 18:14:42 +1100

    SetTimeout logic for flow

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function showNumber() {
-    var x = document.getElementById("inputNumber");
-    document.getElementById("numberDisplay").innerText = x.value;
+    var inputNumber = document.getElementById("inputNumber");
+
+    // hide number input
+    inputNumber.style.display = 'none';
+    document.getElementById("guessBtn").style.display = 'none';
 
     // run loading bar
+    const loadingBar = document.getElementById("loadingBar");
     const bar = document.getElementById("bar");
+    loadingBar.style.display = 'block';
     var width = 1;
     var id = setInterval(frame, 15);
 
@@ -15,4 +20,25 @@ function showNumber() {
             bar.style.width = width + '%';
         }
     }
+
+    // hide loading bar after completion
+    setTimeout(() => {
+        loadingBar.style.display = 'none';
+    }, 2000)
+
+    // show loading circle
+    const loadingCircle = document.getElementById("loadingCircle");
+
+    setTimeout(() => {
+        loadingCircle.style.display = 'block';
+    }, 2000)
+
+    // hide loading circle again
+    setTimeout(() => {
+        loadingCircle.style.display = 'none';
+    }, 4000)
+    
+
+    document.getElementById("numberDisplay").innerText = inputNumber.value;
+
 }
